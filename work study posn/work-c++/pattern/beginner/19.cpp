e0544d71f00dab240bcc5c2d6055514105233891
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-06 23:38:16 +0700

    -

--- work study posn/work-c++/pattern/beginner/19.cpp
+++ work study posn/work-c++/pattern/beginner/19.cpp
@@ -4,21 +4,33 @@ int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int n; cin >> n;
-    for (int i = 0; i < n; i++)
+    int cal = -1;
+    for (int i = n-1; i <= (n*2)-2; i++)
     {
-        for (int j = -n-4; j < n+4; j++)
+        int count = 0;
+        for (int j = -n-4; j < n+5; j++)
         {
-            cout << "(" << i << "," << j << ")";
-            // if (abs(i) == abs(j) || abs(i) > abs(j))
-            // {
-            //     cout << "*";
-            // }
-            // else
-            // {
-            //     cout << " ";
-            // }
+            // cout << "(" << i << "," << j << ")";
+            if (abs(i) == abs(j) || abs(i) > abs(j))
+            {
+                cout << "*"; count++;
+            }
+            else if (count == (n*2)+cal)
+            {
+                for (int k = 0; k <= i-3; k++)
+                {
+                    cout << "*";
+                }
+                break;
+            }
+            else
+            {
+                cout << " ";
+            }
         }
+        cal++;
         cout << endl;
+        // cout << "count : " << count;
     }
     // for (int i = 0; i < n; i++)
     // {
