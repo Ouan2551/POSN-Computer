571ede00f15700ec1530796b1fa2384a4e45073c
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-08 08:23:42 +0700

    -

--- c_learn_self/1.test2.cpp
+++ c_learn_self/1.test2.cpp
@@ -2,11 +2,6 @@
 using namespace std;
 int main()
 {
-    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int n = 1;
-    while (n < 100000000)
-    {
-        cout << "เกด ";
-    }
+    cout << "p";
     return 0;
 }
