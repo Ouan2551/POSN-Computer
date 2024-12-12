cb80a135863ec80ebcac85bbc0185a431b612cda
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-12 22:47:01 +0700

    -

--- work study posn/test/test 1/One-handed_hacker.cpp
+++ work study posn/test/test 1/One-handed_hacker.cpp
@@ -1,54 +1,88 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+char check_left(string a, int size)
+{
+    int chk_left = 0;
+    char left[15] = {'q', 'w', 'e', 'r', 't', 'a', 's', 'd', 'f', 'g', 'z', 'x', 'c', 'v', 'b'};
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < 15; j++)
+        {
+            if (a[i] == left[j])
+            {
+                chk_left++;
+            }
+        }
+    }
+    if (chk_left == size)
+    {
+        return '1';
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+char check_right(string a, int size)
+{
+    int chk_right = 0;
+    char right[11] = {'y', 'u', 'i', 'o', 'p', 'h', 'j', 'k', 'l', 'n', 'm'};
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < 15; j++)
+        {
+            if (a[i] == right[j])
+            {
+                chk_right++;
+            }
+        }
+    }
+    if (chk_right == size)
+    {
+        return '2';
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 int main()
 {
-    int n;
-    cin >> n;
-    string a[101];
-    // เปลี่ยนจาก string เป็น char array สำหรับตัวอักษรฝั่งซ้ายและขวา
-    char left[15] = {'q', 'w', 'e', 'r', 't', 'a', 's', 'd', 'f', 'g', 'z', 'x', 'c', 'v', 'b'};
-    char right[11] = {'y', 'u', 'i', 'o', 'p', 'h', 'j', 'k', 'l', 'n', 'm'};
-
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n; cin >> n; string a[n] = {""};
     for (int i = 0; i < n; i++)
     {
         cin >> a[i];
     }
-
-    for (int m = 0; m < n; m++)
+    for (int i = 0; i < n; i++)
     {
-        bool found = false;
-        // ตรวจสอบตัวอักษรฝั่งซ้าย
-        for (int j = 0; j < 15; j++)
-        {
-            if (a[m][0] == left[j])
-            {
-                cout << "LEFT" << endl;
-                found = true;
-                break;
-            }
-        }
-
-        // ตรวจสอบตัวอักษรฝั่งขวา
-        if (!found)
-        {
-            for (int j = 0; j < 11; j++)
-            {
-                if (a[m][0] == right[j])
-                {
-                    cout << "RIGHT" << endl;
-                    found = true;
-                    break;
-                }
-            }
-        }
-
-        // กรณีไม่เจอในทั้งสองฝั่ง
-        if (!found)
+        int m = a[i].size();
+        if (m == 1)
         {
             cout << "BOTH" << endl;
         }
+        else if(m > 1)
+        {
+            if (check_left(a[i], m) == '1' && check_right(a[i], m) == '2')
+            {
+                cout << "NONE" << endl;
+            }
+            else if (check_left(a[i], m) == '1')
+            {
+                cout << "LEFT" << endl;
+            }
+            else if (check_right(a[i], m) == '2')
+            {
+                cout << "RIGHT" << endl;
+            }
+        }
     }
-
     return 0;
 }
+
+//problem and how fix
+//use char instead string data types because return single character
+//not output all of LEFT RIGHT NONE text
