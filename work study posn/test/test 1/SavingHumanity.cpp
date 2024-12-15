5b06974b3f92dc695ead82f70a9d446c30009790
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-15 08:30:04 +0700

    -

--- work study posn/test/test 1/SavingHumanity.cpp
+++ work study posn/test/test 1/SavingHumanity.cpp
@@ -1,9 +1,29 @@
 #include <bits/stdc++.h>
 using namespace std;
-string detect_ai_intrusion(string code)
+bool detect_ai_intrusion(string code)
 {
     char parentheses[6] = {'{', '}', '(', ')', '[', ']'};
-    int m = code.size();
+    int chk_parentheses = 0; int m = code.size();
+    bool value = true;
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < 6; j++)
+        {
+            if (code[i] == parentheses[j])
+            {
+                chk_parentheses++;
+            }
+        }
+        
+    }
+    if (chk_parentheses % 2 == 0)
+    {
+        return value;
+    }
+    else
+    {
+        value = false; return value;
+    }
 }
 int main()
 {
@@ -17,8 +37,16 @@ int main()
     for (int i = 0; i < count; i++)
     {
         string code = a[i];
-        string value = detect_ai_intrusion(code);
+        bool value = detect_ai_intrusion(code);
+        if (value == true)
+        {
+            cout << "Human Safe" << endl;
+        }
+        else
+        {
+            cout << "AI Intrusion Detected" << endl;
+        }
     }
-    
+    cout << "count : " << count << endl;
     return 0;
 }
