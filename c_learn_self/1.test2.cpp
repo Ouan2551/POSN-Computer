5a19f740102b3b86c260955c3d3306e48233a2ac
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-20 22:30:03 +0700

    -

--- c_learn_self/1.test2.cpp
+++ c_learn_self/1.test2.cpp
@@ -2,6 +2,26 @@
 using namespace std;
 int main()
 {
-    cout << "p";
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int count = 0; cin >> count;
+    for (int i = 2; i <= count; i++)
+    {
+        bool prime_number = true;
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                prime_number = false; break;
+            }
+        }
+        if (prime_number == true)
+        {
+            cout << i << " is prime number." << endl;
+        }
+        else if (prime_number == false)
+        {
+            cout << i << "isn't prime number." << endl;
+        }
+    }
     return 0;
 }
