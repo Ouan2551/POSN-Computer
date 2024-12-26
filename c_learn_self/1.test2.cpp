4ede77bf8f975b5d07b79e0499a8ae479eff64e9
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-26 15:26:10 +0700

    -

--- c_learn_self/1.test2.cpp
+++ c_learn_self/1.test2.cpp
@@ -4,13 +4,26 @@ int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int count; cin >> count;
-    for (int i = 0; i < count; i++)
+    for (int i = 0; i <= count - 1; i++)
     {
-        for (int j = count - i + 1; j > 0; j--)
+        for (int j = 0; j < count - i; j++)
         {
             cout << " ";
         }
         for (int j = 0; j <= i; j++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = 0; i <= count - 2; i++)
+    {
+        for (int j = 0; j <= i + 1; j++)
+        {
+            cout << " ";
+        }
+        for (int j = 0; j < (count - 1) - i; j++)
         {
             cout << "*";
         }
