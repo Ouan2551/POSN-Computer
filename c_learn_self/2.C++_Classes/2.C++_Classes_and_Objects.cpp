424f76691462dea4b16feadff654ae34cd316a0f
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-29 08:40:08 +0700

    -

--- c_learn_self/2.C++_Classes/2.C++_Classes_and_Objects.cpp
+++ c_learn_self/2.C++_Classes/2.C++_Classes_and_Objects.cpp
@@ -1,7 +1,17 @@
 #include <bits/stdc++.h>
 using namespace std;
+// this is how to creat class
+class MyClass {       // The class
+  public:             // Access specifier
+    int myNum;        // Attribute (int variable)
+    string myString;  // Attribute (string variable)
+};
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+    // C++ is associated with classes and objects
+    // example: in real life, a car is an object. The car has attributes, such as weight and color
+
     return 0;
 }
