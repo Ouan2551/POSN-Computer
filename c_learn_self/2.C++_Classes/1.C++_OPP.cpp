bf4075df1df3a7d0e47034b100636c0244d93978
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-29 08:36:11 +0700

    -

---
+++ c_learn_self/2.C++_Classes/1.C++_OPP.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    // OOP stands for Object-Oriented Programming
+
+    // OOP is faster and easier to execute
+    // OOP provides a clear structure for the programs
+    // OOP helps to keep the C++ code DRY "Don't Repeat Yourself"
+    // and makes the code easier to maintain, modify and debug
+    // OOP makes it possible to create full reusable applications with less code
+    // and shorter development time
+
+    // class vs object
+    // fruit |  Apple Banana Mango
+    // Car   |  Toyota Volvo Audi
+
+    // class is a template for object and object is instance of class
+    return 0;
+}
