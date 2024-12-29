41dc6842cfb3db5b54bf7e0399b20754edd02cb4
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-29 09:19:19 +0700

    -

--- c_learn_self/2.C++_Classes/2.C++_Classes_and_Objects.cpp
+++ c_learn_self/2.C++_Classes/2.C++_Classes_and_Objects.cpp
@@ -1,17 +1,46 @@
 #include <bits/stdc++.h>
 using namespace std;
 // this is how to creat class
+// but in this lesson i will use mutiple class
 class MyClass {       // The class
   public:             // Access specifier
     int myNum;        // Attribute (int variable)
     string myString;  // Attribute (string variable)
+}; // Don't forget semicolon ";" over there
+
+class Car {
+  public:
+    string brand;   
+    string model;
+    int year;
 };
+
 int main()
 {
-    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
-    // C++ is associated with classes and objects
-    // example: in real life, a car is an object. The car has attributes, such as weight and color
+  // C++ is associated with classes and objects
+  // example: in real life, a car is an object. The car has attributes, such as weight and color
 
-    return 0;
+  // Creat class
+  MyClass myObj;  // Create an object of 
+  Car myObj1; // after you creat name of class you can't use same name of class
+
+  // Access attributes and set values
+  myObj.myNum = 15; 
+  myObj.myString = "Tom";
+
+  myObj1.brand = "Ferrari";
+  myObj1.model = "Super Sport";
+
+  // Print attribute values
+  cout << myObj.myNum << endl;
+  cout << myObj.myString << endl;
+
+  cout << myObj1.brand << endl;
+  cout << myObj1.model;
+
+  // What is the role of a class in C++?
+  // Ans. To define a template or blueprint for creating objects
+  return 0;
 }
---
+++ c_learn_self/2.C++_Classes/3.C++_Class_Methods.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Myclass
+{
+    // you can creat function in class like creat outside function (easily to do blud)
+    public:
+        void Output_txt() // creat function in class
+        {
+            cout << "hello world" << endl;
+        }
+}; // don't forget semicolon overthere
+
+
+class People // The class
+{
+public:              // Access specifier
+    void myMethod();   // Method/function declaration
+};
+
+// Method/function definition outside the class
+void People::myMethod()
+{
+    cout << "European People";
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+    // creat object
+    Myclass myobj;
+    People myobj1;
+
+    myobj.Output_txt(); // call function inside class output text
+    myobj1.myMethod(); // call function method
+
+    return 0;
+
+    // C++ Class Methods
+    // Ans Inside or outside the class definition
+}
