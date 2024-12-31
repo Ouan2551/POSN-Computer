ac9a371490b3f07a75f846ee9f638474dafd178c
Ouan2551 <Nannaphat2110@gmail.com>
2024-12-31 09:15:26 +0700

    -

--- c_learn_self/1.C++_Basics/16.C++_Pointer.cpp
+++ c_learn_self/1.C++_Basics/16.C++_Pointer.cpp
@@ -1,22 +1,41 @@
-#include <iostream>
-#include <string>
+#include <bits/stdc++.h>
 using namespace std;
+int main()
+{
+  ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
 
-int main() {
-  string food = "KFC";  // A string variable
-  string* ptr = &food;  // A pointer variable that stores the address of food
+  // Creating Pointers
+  string food = "KFC";  // just string variable
+  string* ptr = &food;
+  // pointer variable that stores the address of food
+  // prt just variable to store memory address
 
-    // Output the value of food
-    cout << food << "\n";
+  // Output the value of food
+  cout << "food : " << food << "\n";
 
-    // Output the memory address of food
-    cout << &food << "\n";
+  // Output the memory address of food
+  cout << "&food : " << &food << "\n";
 
-    // Output the memory address of food with the pointer
-    cout << ptr << "\n";
+  // Output the memory address of food with the pointer
+  cout << "ptr : " << ptr << "\n";
 
-    // Output value
-    cout << *ptr << endl;
+  // Output value
+  cout << "*ptr : " << *ptr << endl;
 
-    return 0;
+  //change the value of the pointer
+  *ptr = "Pizza";
+
+  //output new value of pointer
+  cout << "*ptr : " << *ptr << endl;
+
+  //output memory address new value
+  cout << "ptr : " << ptr << endl;
+
+  //and then you will get new value of variables name food
+  cout << "food : " << food << endl;
+
+  return 0;
+
+  // What does a pointer store in C++
+  // The memory address of another variable
 }
