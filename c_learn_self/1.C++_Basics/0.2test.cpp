88a3834cc759f1f64b6da555f07e730a86d79a9f
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-08 09:01:37 +0700

    -

--- c_learn_self/1.C++_Basics/0.2test.cpp
+++ c_learn_self/1.C++_Basics/0.2test.cpp
@@ -3,5 +3,46 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int count; cin >> count;
+    for (int i = -count; i < -1; i++)
+    {
+        for (int j = -count; j < count + 1; j++)
+        {
+            // cout << "(" << i << "," << j << ")";
+            if (abs(i) == abs(j))
+            {
+                cout << "*";
+            }
+            else if (abs(i) == 1)
+            {
+                break;
+            }
+            else
+            {
+                cout << "-";
+            }
+        }
+        cout << endl;
+    }
+    for (int i = 2; i < count; i++)
+    {
+        for (int j = -count; j < count + 1; j++)
+        {
+            // cout << "(" << i << "," << j << ")";
+            if (abs(i) == abs(j))
+            {
+                cout << "*";
+            }
+            else if (abs(i) == 1)
+            {
+                break;
+            }
+            else
+            {
+                cout << "-";
+            }
+        }
+        cout << endl;
+    }
     return 0;
 }
