3656b1161121fd42472f286955a26bec8e70914e
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-11 09:36:31 +0700

    -

--- c_learn_self/1.C++_Basics/0.2test.cpp
+++ c_learn_self/1.C++_Basics/0.2test.cpp
@@ -4,13 +4,43 @@ int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int count; cin >> count;
-    for (int i = count; i > -1; i--)
+    for (int i = count; i > 1; i--)
     {
         for (int j = -count; j < count + 1; j++)
         {
+            // cout << '(' << i << ',' << j << ')';
+            if (abs(i) == abs(j) || abs(i) < abs(j) || abs(i) == 1)
+            {
+                cout << '*';
+            }
+            else if (abs(j) == 0 || j == 1)
+            {
+                continue;
+            }
+            else
+            {
+                cout << ' ';
+            }
+        }
+        cout << '\n';
+    }
+    for (int i = 0; i < count*2 - 1; i++)
+    {
+        cout << '*';
+    }
+    cout << '\n';
+    for (int i = 2; i < count + 1; i++)
+    {
+        for (int j = -count; j < count + 1; j++)
+        {
+            // cout << '(' << i << ',' << j << ')';
             if (abs(i) == abs(j) || abs(i) < abs(j))
             {
                 cout << '*';
+            }
+            else if (abs(j) == 0 || j == 1)
+            {
+                continue;
             }
             else
             {
