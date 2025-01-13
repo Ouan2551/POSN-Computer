d6bcbe499ec14504dadf6fb1836a26cc15a5f084
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-14 06:26:14 +0700

    -

--- work study posn/work-c++/morning test/22.Tee-Yod2.cpp
+++ work study posn/work-c++/morning test/22.Tee-Yod2.cpp
@@ -3,14 +3,6 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int count = 0; cin >> count; string a[count] = {""};
-    for (int i = 0; i < count; i++)
-    {
-        cin >> a[i];
-    }
-    for (int i = 0; i < count; i++)
-    {
-        cout << a[i] << '\n';
-    }
+    cout << "Hello world";
     return 0;
 }
