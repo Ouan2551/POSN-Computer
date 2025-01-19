9a0c8af6368c8c6a8dd78860ae448aff84b59122
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-19 22:33:47 +0700

    -

--- c_learn_self/1.C++_Basics/11.C++_Switch.cpp
+++ c_learn_self/1.C++_Basics/11.C++_Switch.cpp
@@ -3,5 +3,43 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    // switch use like if else
+
+    // this it structure switch
+    // switch (expression)
+    // {
+    // case /* constant-expression */:
+    //     /* code */
+    //     break;
+    
+    // default:
+    //     break;
+    // }
+
+    int day = 4;
+switch (day) {
+    case 1:
+    cout << "Monday";
+    break;
+    case 2:
+    cout << "Tuesday";
+    break;
+    case 3:
+    cout << "Wednesday";
+    break;
+    case 4:
+    cout << "Thursday";
+    break;
+    case 5:
+    cout << "Friday";
+    break;
+    case 6:
+    cout << "Saturday";
+    break;
+    case 7:
+    cout << "Sunday";
+    break;
+}
+
     return 0;
 }
