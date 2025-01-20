ff6d02e778aad4f20c0f308c7c9793e531e14b68
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-20 22:42:32 +0700

    -

---
+++ work study posn/2.work-c++/question/6.Array2D_Matrix_multiply.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int count; cin >> count; string a[count];
+    string txt = "LuvYou"; 
+    for (int i = 0; i < count; i++)
+    {
+        cin >> a[i];
+    }
+
+    // check text
+    for (int i = 0; i < count; i++)
+    {
+        string chk_text = a[i]; int chk_nums = chk_text.size();
+        int equal = 0;
+        for (int j = 0; j < chk_nums; j++)
+        {
+            for (int k = 0; k < 6; k++)
+            {
+                if (chk_text[j] == txt[k])
+                {
+                    equal++;
+                }
+            }
+        }
+
+        if (equal == chk_nums)
+        {
+            cout << "YES" << '\n';
+        }
+        else
+        {
+            cout << "NO" << '\n';
+        }
+        cout << "Equal : " << equal << '\n';
+    }
+    return 0;
+}
+// idea now
+// use if else to continue check next to until complete string
