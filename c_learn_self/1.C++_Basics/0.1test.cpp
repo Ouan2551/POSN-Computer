edc90f29f1ffcf70f2f0ae7bfe9f6e7ea629f1ac
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-20 21:56:36 +0700

    -

--- c_learn_self/1.C++_Basics/0.1test.cpp
+++ c_learn_self/1.C++_Basics/0.1test.cpp
@@ -3,9 +3,5 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    string txt; getline(cin, txt); int m = txt.size();
-    for (int i = m - 1; i >= 0; i--)
-    {
-        cout << txt[i];
-    }
+    return 0;
 }
