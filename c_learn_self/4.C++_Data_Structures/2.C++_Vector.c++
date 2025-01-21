ebf5a11dcc4299b604562d0baab61ddc39f7249c
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-21 22:25:48 +0700

    -

--- c_learn_self/4.C++_Data_Structures/2.C++_Vector.c++
+++ c_learn_self/4.C++_Data_Structures/2.C++_Vector.c++
@@ -1,24 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 int main()
-#define windows ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0)
 {
-    vector <int> vector; // name variable
-    //this verctor line 6 it can declare size or not declare size
-    vector.push_back(10);
-    vector.push_back(20);
-    //use same like array
-    for (int i = 0; i < vector.size(); i++)
-    {
-        cout << vector[i] << endl;
-    }
-    vector.clear();
-    cout << vector.size();
-    
-    //auto output but it make don't know where of it
-    for (auto : vector)
-    {
-        cout << i << endl;
-    }
+    ios::sync_with_stdio(0):cin.tie(0);cout.tie(0);
     return 0;
 }
--- work study posn/2.work-c++/question/6.Array2D_Matrix_multiply.cpp
+++ work study posn/2.work-c++/question/6.Array2D_Matrix_multiply.cpp
@@ -5,18 +5,18 @@ int main()
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
     int m1, m2, n1, n2; cin >> m1 >> n1; int a[m1][n1] = {0};
     m1 = n1 = m2 = n2 = 0;
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m1; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < n1; j++)
         {
             cin >> a[i][j];
         }
     }
 
     cin >> m2 >> n2; int b[m2][n2] = {0};
-    for (int i = 0; i < m; i++)
+    for (int i = 0; i < m2; i++)
     {
-        for (int j = 0; j < n; j++)
+        for (int j = 0; j < n2; j++)
         {
             cin >> b[i][j];
         }
@@ -26,11 +26,11 @@ int main()
     if (n1 == m2)
     {
         int c[m1][n2] = {0};
-        for (int i = 0; i < m; i++)
+        for (int i = 0; i < m1; i++)
         {
-            for (int j = 0; j < n; j++)
+            for (int j = 0; j < n2; j++)
             {
-                c[i][j] == 
+                c[i][j] == a[i][j] * b[i][j]
             }
         }
     }
