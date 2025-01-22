50d4fa63d0260fc9eca0a8e9d965fdddee88e19d
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-22 21:16:01 +0700

    -

---
+++ work study posn/2.work-c++/question/7.Martial Arts.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int count; cin >> count; int a[count];
+    for (int i = 0; i < count; i++)
+    {
+        cin >> a[i];
+    }
+    
+    return 0;
+}
