e1b000f6151d4451ec71a8b1e0127db9c9e4a8bb
Ouan2551 <Nannaphat2110@gmail.com>
2025-01-26 22:38:33 +0700

    -

--- c_learn_self/1.C++_Basics/0.1test.cpp
+++ c_learn_self/1.C++_Basics/0.1test.cpp
@@ -3,5 +3,11 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n; long long result = 1; cin >> n;
+    for (int i = n; i > 0; i--)
+    {
+        result *= i;
+    }
+    cout << "factorial result : " << result;
     return 0;
 }
