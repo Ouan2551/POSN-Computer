75b11082d16724112855f09cff56d073791bc887
Ouan2551 <Nannaphat2110@gmail.com>
2025-02-19 18:13:29 +0700

    -

---
+++ work study posn/2.work-c++/question/15.Array2D_Matrix_Transpose.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int m,n; cin >> m >> n; int a[m][n];
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cin >> a[i][j];
+        }
+    }
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << a[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    cout << '\n';
+    for (int i = 0; i < m; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            cout << a[i][j] << ' ';
+        }
+        cout << '\n';
+    }
+    
+    return 0;
+}
