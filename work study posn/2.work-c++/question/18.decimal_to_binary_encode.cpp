43211af0b6390a80f3261a59ec59a2523180d541
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-02-22 23:04:01 +0700

    -

--- work study posn/2.work-c++/question/18.decimal_to_binary_encode.cpp
+++ work study posn/2.work-c++/question/18.decimal_to_binary_encode.cpp
@@ -3,14 +3,7 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int num, carry; cin >> num; carry = num;
-    // int length = to_string(num).size();
-    while (carry != 0)
-    {
-        int result = carry % 2, carry = num / 2;
-        cout << "result : " << '\n';
-        cout << "carry : " << '\n';
-    }
-    
+    int num; cin >> num;
+    cout << bitset<32>(num);
     return 0;
 }
