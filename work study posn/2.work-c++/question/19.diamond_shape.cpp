bc5f1ee906c7c5dbfd19f5bb4f993f134e21c79b
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-02-22 23:23:16 +0700

    -

---
+++ work study posn/2.work-c++/question/19.diamond_shape.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int count; cin >> count;
+    if (count == 3)
+    {
+        cout << "#+#" << '\n';
+        cout << "+++" << '\n';
+        cout << "#+#" << '\n';
+        return 0;
+    }
+    else
+    {
+        for (int i = 0; i <= count - 3; i++)
+        {
+            for (int j = -count + 4; j <= count - 4; j++)
+            {
+                // cout << "(" << i << "," << j << ')';
+                if (abs(i) == abs(j) || abs(i) > abs(j))
+                {
+                    cout << '*';
+                }
+                else
+                {
+                    cout << '#';
+                }
+            }
+            cout << '\n';
+        }
+        for (int i = -count + 4; i <= 0; i++)
+        {
+            for (int j = -count + 4; j <= count - 4; j++)
+            {
+                // cout << "(" << i << "," << j << ')';
+                if (abs(i) == abs(j) || abs(i) > abs(j))
+                {
+                    cout << '*';
+                }
+                else
+                {
+                    cout << '#';
+                }
+            }
+            cout << '\n';
+        }
+    }
+    return 0;
+}
