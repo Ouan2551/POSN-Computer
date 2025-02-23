7b753ab24d3c9d160d876a969e83356b902d40c7
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-02-23 23:15:00 +0700

    -

---
+++ work study posn/2.work-c++/question/20.Dr_Strange_compressed.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int count, time; cin >> count >> time; long long int = find;
+    for (int i = 1; i <= count; i++)
+    {
+        find 
+    }
+    return 0;
+}
