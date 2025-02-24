d86bbd25a91fcaaa1359fe5d04961a991dc8fab4
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-02-24 16:56:51 +0700

    -

--- work study posn/2.work-c++/question/20.Dr_Strange_compressed.cpp
+++ work study posn/2.work-c++/question/20.Dr_Strange_compressed.cpp
@@ -3,10 +3,22 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int count, time; cin >> count >> time; long long int = find;
+    int count;
+    string time; cin >> count >> time; string find = "";
     for (int i = 1; i <= count; i++)
     {
-        find 
+        find = find + time;
     }
+    int m = find.size(), n = time.size();
+    for (int i = 0; i < m; i++)
+    {
+        if ()
+        {
+            /* code */
+        }
+        
+    }
+    
+    cout << find << '\n';
     return 0;
 }
