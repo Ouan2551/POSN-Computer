b6d2623abb56476fe2c8512b2612239941da2f0e
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-02-25 22:37:59 +0700

    -

--- work study posn/2.work-c++/question/20.1.Dr_Strange_compressed.cpp
+++ work study posn/2.work-c++/question/20.1.Dr_Strange_compressed.cpp
@@ -1,25 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// solution from chat_gbt
+
 // Function to compute prefix function (pi array)
-vector<int> computePrefixFunction(string s) {
+vector<int> computePrefixFunction(string s)
+{
     int n = s.size();
     vector<int> pi(n, 0);
-    for (int i = 1, j = 0; i < n; i++) {
+    for (int i = 1, j = 0; i < n; i++)
+    {
+        // just check character
         while (j > 0 && s[i] != s[j])
+        {
             j = pi[j - 1];
+        }
         if (s[i] == s[j])
+        {
             j++;
+        }
         pi[i] = j;
     }
     return pi;
 }
 
-int main() {
-    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-    int n;
-    string s;
-    cin >> n >> s;
+int main()
+{
+    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    int n; string s; cin >> n >> s;
 
     vector<int> pi = computePrefixFunction(s);
     int period = n - pi[n - 1]; // Smallest period
