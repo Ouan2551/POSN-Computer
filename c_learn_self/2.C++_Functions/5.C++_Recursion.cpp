1dd0d4b49fe09755cef1d62681ad05832525772b
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-03-01 20:43:03 +0700

    -

--- c_learn_self/2.C++_Functions/5.C++_Recursion.cpp
+++ c_learn_self/2.C++_Functions/5.C++_Recursion.cpp
@@ -1,7 +1,23 @@
 #include <bits/stdc++.h>
-using namespaces std;
+using namespace std;
+int sum(int num)
+{
+    if (num > 0)
+    {
+        return num + sum(num - 1);
+    }
+    else
+    {
+        return 0;
+    }
+}
+
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+    // Recursion => make function call itself
+    // Example (adding a range of number)
+    int result = sum(10); // 10 + 9 + 8 + 7 + 6 + 5 + 4 + 3 + 2 + 1
+    cout << result;
     return 0;
 }
