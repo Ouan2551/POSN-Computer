780c19a7ea02c66b62ad867f2dbab3c1b06cb9a8
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-03-01 22:51:10 +0700

    -

--- c_learn_self/4.C++_Data_Structures/3.C++_List.cpp
+++ c_learn_self/4.C++_Data_Structures/3.C++_List.cpp
@@ -3,20 +3,52 @@ using namespace std;
 int main()
 {
     ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    // list => similar vector but can store mutiple same type
-    // 1) add and remove value from front and end of list but vector use for end only
-    // 2) can not use specific index or access value by index numbers
+    // list is similar vector but it have 2 thing different
+    // 1) can add and remove member from start and at the end
+    // 2) list not support random access member it need to use index to access
 
-    // use #include <list> to use list in program
+    // to use list use this
+    // #include <list>
 
-    // declare list
-    // list<data_type> name_variables;
-    list <string> cars;
-    list <string> cars1 = {"Ferrari", "Lamborghini", "Porsche", "Pagani"};
-    for (string i : cars1)
+    // creat list
+    // list<data_type> name_list;
+    list<int> nums = {1, 2, 3, 4, 5};
+
+    for (int i : nums)
     {
         cout << i << '\n';
     }
     
+    // get first member list
+    cout << nums.front() << ' ';
+    // change value first member
+    nums.front() = 10;
+    cout << nums.front() << '\n';
+
+    // get last member list
+    cout << nums.back() << ' ';
+    nums.back() = 20;
+    cout << nums.back() << '\n';
+
+    // add member list
+    // add front
+    nums.push_front() = 15;
+
+    // add last
+    nums.push_back() = 25;
+
+    // remove member list
+    // remove front
+    nums.pop_front();
+
+    // remove back
+    nums.pop_back();
+
+    // list size => show count member in list
+    cout << nums.size() << '\n';
+
+    // check list is empty or not
+    cout << nums.empty() << '\n';
+    // have 2 value '0' (false) and '1' (true)
     return 0;
 }
