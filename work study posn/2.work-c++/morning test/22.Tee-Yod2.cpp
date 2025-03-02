2db3a559fecd75809f370c79ed2284a7b0377c8f
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-03-02 22:53:34 +0700

    -

--- work study posn/2.work-c++/morning test/22.Tee-Yod2.cpp
+++ work study posn/2.work-c++/morning test/22.Tee-Yod2.cpp
@@ -2,12 +2,14 @@
 using namespace std;
 int main()
 {
-    ios::sync_with_stdio(0);cin.tie(0);cout.tie(0);
-    int count; cin >> count; string a[count];
-    vector<char> b; cin.ignore();
-    // clear the newline character from the input buffer
-    // before using getline()
+    ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    
+    int count = 0;
+    cin >> count;
+    cin.ignore(); // FIX: Ignore the leftover newline
 
+    string a[count];
+    
     for (int i = 0; i < count; i++)
     {
         getline(cin, a[i]);
@@ -15,27 +17,8 @@ int main()
 
     for (int i = 0; i < count; i++)
     {
-        // int m = a[i].size();
-        // for (int j = 0; j < m; j++)
-        // {
-        //     cout << a[i][j] << '\n';
-        // }
-        if (a[i][0] == 'b')
-        {
-            // index 4 and 5
-            b.push_back(a[i][4]); b.push_back(a[i][5]);
-        }
-        else if (a[i][0] == 'c')
-        {
-            // index 7 and 8
-            b.push_back(a[i][7]); b.push_back(a[i][8]);
-        }
+        cout << a[i] << '\n';
     }
-    int n = b.size();
-    for (int i = 0; i < n; i++)
-    {
-        cout << "wow : " << b[i] << '\n';
-    }
-    
+
     return 0;
 }
