6ea2ec568be1396560ed092d93c4fc14c35c5bd8
🦜Nannaphat Songduong <121873082+Ouan2551@users.noreply.github.com>
2025-03-24 00:49:17 +0700

    -

---
+++ work study posn/2.work-c++/question/26.Flood.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios::sync_with_stdio(0):cin.tie(0);cout.tie(0);
+    int count; cin >> count; int a[count] = {0}, b[count-1] = {0};
+    for (int i = 0; i < count; i++)
+    {
+        cin >> a[i];
+    }
+    for (int i = 0; i < count; i++)
+    {
+    }
+    
+    return 0;
+}
